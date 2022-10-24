ff75f20914c9219fc091e422aa779e09317b9e86
Art <you@example.com>
2022-10-24 18:28:43 +0200

    Converted class compononent to functional component for films

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import FilmCard from './components/films/FilmCard'
+import FilmCardFunctional from "./components/films/FilmCardFunctional";
 
 function App () {
   return <>
     <div className="container mt-5">
       <Header />
-      <FilmCard />
+      {/*<FilmCard />*/}
+      <FilmCardFunctional />
     </div>
   </>
 }
