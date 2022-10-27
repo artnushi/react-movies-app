4737d5a7b86a59973e04fe7e21f88b00c4e1d4f7
Drin <109246823+DrinMarevci@users.noreply.github.com>
2022-10-27 12:57:33 +0200

    Revert "Fixing errors in Date Picker"

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -3,30 +3,32 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import * as PropTypes from "prop-types";
 
-export const DatePickerInput = ({ value, setValues, values }) => {
-  const [startDate, setStartDate] = useState(new Date());
+export const DatePickerInput = ({value, setValues, values}) => {
+    const [startDate, setStartDate] = useState(new Date());
 
-  const handleChange = (value) => {
-    setStartDate(value);
-    setValues({ ...values, date_of_birth: value });
-  };
+    const handleChange = (value) => {
+        setStartDate(value);
+        setValues({...values, date_of_birth: value});
+    };
 
-  return (
-    <>
-      <span>Date of birth</span>
-      <DatePicker
-        className={"form-control"}
-        selected={startDate}
-        dateFormat="MMMM d, yyyy"
-        onChange={handleChange}
-        value={value}
-      />
-    </>
-  );
+    return (
+        <>
+            <span>Date of birth</span>
+            <DatePicker
+                className={'form-control'}
+                selected={startDate}
+                dateFormat="MMMM d, yyyy"
+                onChange={handleChange}
+                value={value}
+
+            />
+        </>
+    );
 };
 
 DatePickerInput.propTypes = {
-  value: PropTypes.string,
-  setValues: PropTypes.func,
-  values: PropTypes.object,
-};
+    value: PropTypes.string,
+    setValues: PropTypes.string,
+    values: PropTypes.string,
+
+}
