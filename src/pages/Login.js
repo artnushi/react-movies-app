4ee169d221d8731c708d6df2f264dad95f1030d0
mucolliardit@gmail.com <mucolliardit@gmail.com>
2022-10-27 16:40:30 +0200

    Finishing login form and implement axios

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import Validation from "../components/Validation";
 import "../App.css";
 import HeaderPage from "../components/header/HeaderPage";
+import axios from "axios";
 
 export default function SignUp() {
   const [values, setValues] = useState({
-    fullname: "",
+    first_name: "",
+    last_name: "",
     email: "",
     password: "",
+    date_of_birth: "",
   });
 
   const [errors, setErrors] = useState({});
@@ -22,48 +25,63 @@ export default function SignUp() {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    setErrors(Validation(values));
+
+    axios
+      .get("https://flowrspot-api.herokuapp.com/api/v1/users/register", values)
+      .then(function (response) {
+        console.log("success", response);
+      })
+      .catch(function (error) {
+        // console.log('error', error.response.data.error);
+      });
+
+    console.log("values", values);
+    // setErrors(Validation(values));
   };
   return (
     <>
       <div className="container">
         <HeaderPage />
         <div className="card gap-3 m-5">
-
           <div>
             <h2 className="title m-5">Login</h2>
           </div>
-          <form className="form-wrapper col-4 m-5">
-            <div className="form-check">
-              <label className="label">Email</label>
-              <input
-                className=" form-control"
-                type="text"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-              />
-              {errors.email && <p className="error">{errors.email}</p>}
+          <form className="m-5" onSubmit={handleFormSubmit}>
+            <div className={"row"}>
+              <div className="col-12 col-md-6">
+                <label className="label">Email</label>
+                <input
+                  className=" form-control"
+                  type="text"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                />
+                {errors.email && <p className="error">{errors.email}</p>}
+              </div>
+
+              <div className="col-12 col-md-6">
+                <label className="label">Password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                />
+                {errors.password && <p className="error">{errors.password}</p>}
+              </div>
             </div>
-            <div className="form-check">
-              <label className="label">Password</label>
-              <input
-                className="form-control"
-                type="password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-              />
-              {errors.password && <p className="error">{errors.password}</p>}
-            </div>
-            <div className="text-center">
-              <button
-                type="button"
-                className="btn btn-primary text-center"
-                onClick={handleFormSubmit}
-              >
-                SignUp
-              </button>
+
+            <div className="row">
+              <div className="col-12 col-md-12 my-5">
+                <button
+                  type="submit"
+                  className="btn btn-primary text-center w-100"
+                >
+                  Sign Up
+                </button>
+              </div>
             </div>
           </form>
         </div>
