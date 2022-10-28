5271364f14400c1f2a4b177bc84aeb520b819fd1
Kaltrim Kodraliu <kodraliukaltrim@gmail.com>
2022-10-28 03:12:07 +0200

    Created login page design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,9 +11,7 @@ export default function SignUp() {
   });
 
   const [errors, setErrors] = useState({});
-
   const handleChange = (e) => {
-    console.log(e.target.value);
     setValues({
       ...values,
       [e.target.name]: e.target.value,
@@ -23,51 +21,62 @@ export default function SignUp() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     setErrors(Validation(values));
+    console.log("values", values);
   };
   return (
     <>
       <div className="container">
         <HeaderPage />
-        <div className="card gap-3 m-5">
 
-          <div>
-            <h2 className="title m-5">Login</h2>
-          </div>
-          <form className="form-wrapper col-4 m-5">
-            <div className="form-check">
-              <label className="label">Email</label>
-              <input
-                className=" form-control"
-                type="text"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-              />
-              {errors.email && <p className="error">{errors.email}</p>}
-            </div>
-            <div className="form-check">
-              <label className="label">Password</label>
-              <input
-                className="form-control"
-                type="password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-              />
-              {errors.password && <p className="error">{errors.password}</p>}
-            </div>
-            <div className="text-center">
-              <button
-                type="button"
-                className="btn btn-primary text-center"
-                onClick={handleFormSubmit}
-              >
-                SignUp
-              </button>
-            </div>
-          </form>
+        <div>
+          <h2 className="title row container d-flex justify-content-center">Welcome back</h2>
         </div>
       </div>
+      <form className="m-5" onSubmit={handleFormSubmit}>
+        <div className="row container d-flex justify-content-center">
+          <div className="col-12 col-md-6">
+            <label className="label">Email</label>
+            <input
+              className="form-control"
+              placeholder="Email"
+              type="text"
+              name="first_name"
+              value={values.first_name}
+              onChange={handleChange}
+              required
+            />
+            {errors.first_name && (
+              <p className="error">{errors.first_name}</p>
+            )}
+          </div>
+        </div>
+        <div className="row container d-flex justify-content-center ">
+
+          <div className="col-12 col-md-6 ">
+            <label className="label">Password</label>
+            <input
+              className="form-control"
+              placeholder="Password"
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.password && <p className="error">{errors.password}</p>}
+          </div>
+        </div>
+
+        <div className="row container ">
+          <div className="col-12 col-md-12 my-5 d-flex justify-content-center ">
+            <button
+              type="submit"
+              className="btn btn-primary text-center w-50"
+            >
+              Login to your Account
+            </button>
+          </div>
+        </div>
+      </form>
     </>
   );
 }
