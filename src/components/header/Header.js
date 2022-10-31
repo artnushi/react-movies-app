958ff5b591c6d512b905a0904a6bdbb45bded7b2
Art <you@example.com>
2022-10-31 20:39:15 +0100

    Configured axios, implemented formik validations

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import {SearchContext} from "../../pages/Home";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import PropTypes from "prop-types";
 
 function Header(props) {
+
+
+    let navigate = useNavigate();
+
+    const handleLogout = () => {
+
+        localStorage.removeItem('auth_token')
+        localStorage.setItem('isAuthenticated', false)
+        navigate('/login')
+    }
+
+    const isAuthenticated = JSON.parse( localStorage.getItem('isAuthenticated') );
+
+    console.log('isAuthenticated', isAuthenticated)
     return (
         <>
             <div className="d-flex justify-content-between align-items-center mb-5">
@@ -47,20 +61,30 @@ function Header(props) {
                                 Sign up
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-link">
-                                Log in
-                            </Link>
-                        </li>
+
+                        {
+                            isAuthenticated ?
+                                <>
+                                    <li className="nav-item">
+                                        <button onClick={handleLogout}  className="btn btn-link text-decoration-none">
+                                            Log out
+                                        </button>
+                                    </li>
+                                </>
+                                :
+
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/login" className="nav-link">
+                                            Log in
+                                        </Link>
+                                    </li>
+                                </>
+                        }
 
                         <li className="nav-item">
                             <Link to="/authenticated-user" className="nav-link">
                                 Authenticated user
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/signin" className="nav-link">
-                                SignIn
                             </Link>
                         </li>
                     </ul>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,47 +3,49 @@ import "../App.css";
 import axios from "axios";
 import Layout from "../components/Layout";
 import { useNavigate } from 'react-router-dom';
+import {Form, Formik, Field, withFormik} from "formik";
+import * as Yup from "yup";
+import PropTypes from 'prop-types';
+import api from "../helpers/api";
 
 
-export default function SignUp() {
+const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+        .required('Email is required!')
+        .email('Must be email'),
+    password: Yup
+        .string()
+        .required('Your password is required!')
+        .min(8, "Minimum 8 characters are required"),
+});
+
+
+function Login(props) {
     let navigate = useNavigate();
-    const [values, setValues] = useState({
-        email: "",
-        password: ""
-    });
 
-    const [errors, setErrors] = useState({});
+    const handleFormSubmit = (values) => {
+        console.log(values)
 
-    const handleChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        });
-    };
+        api.post("/users/login", values)
+            .then( (response) => {
 
+                const auth_token = response.data.auth_token
 
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-
-        axios
-            .post("https://flowrspot-api.herokuapp.com/api/v1/users/login", values)
-            .then(function (response) {
-                console.log("success", response);
-
+                localStorage.setItem('auth_token', auth_token)
+                localStorage.setItem('isAuthenticated', true)
 
                 navigate('/authenticated-user')
 
             })
-            .catch(function (error) {
-
+            .catch( (error) => {
                 alert('error, please sign up')
                 navigate('/register')
             });
 
         console.log("values", values);
-        // setErrors(Validation(values));
+
     };
+
     return (
         <>
 
@@ -52,46 +54,70 @@ export default function SignUp() {
                     <div>
                         <h2 className="title m-5">Login</h2>
                     </div>
-                    <form className="m-5" onSubmit={handleFormSubmit}>
-                        <div className={"row"}>
-                            <div className="col-12 col-md-6">
+                    <Formik
+                        initialValues={{
+                            email: "",
+                            password: ""
+                        }}
+                        onSubmit={handleFormSubmit}
+                        validationSchema={LoginSchema}
+                        >
+                        { (props) => (
+                            <Form >
+
+
                                 <label className="label">Email</label>
-                                <input
+                                <Field
                                     className=" form-control"
                                     type="text"
                                     name="email"
-                                    value={values.email}
-                                    onChange={handleChange}
+                                    value={props.values.email}
+                                    onChange={props.handleChange}
                                 />
-                                {errors.email && <p className="error">{errors.email}</p>}
-                            </div>
 
-                            <div className="col-12 col-md-6">
+                                {props.touched.email && props.errors.email && <p className="text-danger">{props.errors.email}</p>}
+
                                 <label className="label">Password</label>
-                                <input
-                                    className="form-control"
+                                <Field
+                                    className=" form-control"
                                     type="password"
                                     name="password"
-                                    value={values.password}
-                                    onChange={handleChange}
+                                    value={props.values.password}
+                                    onChange={props.handleChange}
                                 />
-                                {errors.password && <p className="error">{errors.password}</p>}
-                            </div>
-                        </div>
 
-                        <div className="row">
-                            <div className="col-12 col-md-12 my-5">
-                                <button
-                                    type="submit"
-                                    className="btn btn-primary text-center w-100"
-                                >
-                                    Log in
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+
+                                {props.touched.password && props.errors.password && <p className="text-danger">{props.errors.password}</p>}
+
+
+                                <div className="mt-4 mb-2">
+                                    <button
+                                        disabled={ ! (props.dirty && props.isValid) }
+                                        type="submit"
+                                        className="btn btn-primary text-center w-100"
+                                    >
+                                        Log in
+                                    </button>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
             </Layout>
         </>
     );
 }
+
+Login.propTypes = {
+    dirty: PropTypes.bool,
+    errors: PropTypes.object,
+    handleChange: PropTypes.func,
+    isValid: PropTypes.bool,
+    resetForm: PropTypes.func,
+    touched: PropTypes.object,
+    values: PropTypes.object,
+    handleFormSubmit: PropTypes.func
+}
+
+
+export default Login;
