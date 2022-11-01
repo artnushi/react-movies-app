51d6bb78927908c2f883ff4dc9ea07cc67edee23
mucolliardit@gmail.com <mucolliardit@gmail.com>
2022-11-01 23:29:35 +0100

    Testing application with jest

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,102 +1,97 @@
-import React from 'react'
-import {SearchContext} from "../../pages/Home";
-import {Link, useNavigate} from "react-router-dom";
+import React from "react";
+import { SearchContext } from "../../pages/Home";
+import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function Header(props) {
+  let navigate = useNavigate();
 
+  const handleLogout = () => {
+    localStorage.removeItem("auth_token");
+    localStorage.setItem("isAuthenticated", false);
+    navigate("/login");
+  };
 
-    let navigate = useNavigate();
+  const isAuthenticated = JSON.parse(localStorage.getItem("isAuthenticated"));
 
-    const handleLogout = () => {
+  console.log("isAuthenticated", isAuthenticated);
+  return (
+    <>
+      <div className="d-flex justify-content-between align-items-center mb-5">
+        <Link to="/" className="nav-link fs-1">
+          Home
+        </Link>
 
-        localStorage.removeItem('auth_token')
-        localStorage.setItem('isAuthenticated', false)
-        navigate('/login')
-    }
+        {props.showSearch && (
+          <SearchContext.Consumer>
+            {({ searchValue, handleChange }) => {
+              return (
+                <>
+                  <div>
+                    <div className="mb-3">
+                      <label htmlFor="search-movies" className="form-label">
+                        Search flower
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="search movie"
+                        className="form-control"
+                        name="search_movies"
+                        onChange={handleChange}
+                        value={searchValue}
+                        aria-describedby="search-movies"
+                      />
+                    </div>
+                  </div>
+                </>
+              );
+            }}
+          </SearchContext.Consumer>
+        )}
 
-    const isAuthenticated = JSON.parse( localStorage.getItem('isAuthenticated') );
+        <div>
+          <ul className="nav">
+            <li className="nav-item">
+              <Link to="/register" className="nav-link">
+                Sign up
+              </Link>
+            </li>
 
-    console.log('isAuthenticated', isAuthenticated)
-    return (
-        <>
-            <div className="d-flex justify-content-between align-items-center mb-5">
-                <Link to="/" className="nav-link fs-1">
-                    Home
-                </Link>
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <button
+                    onClick={handleLogout}
+                    className="btn btn-link text-decoration-none"
+                  >
+                    Log out
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    Log in
+                  </Link>
+                </li>
+              </>
+            )}
 
-
-                {props.showSearch &&
-                <SearchContext.Consumer>
-                    {({searchValue, handleChange}) => {
-                        return (
-                            <>
-                                <div>
-                                    <div className="mb-3">
-                                        <label htmlFor="search-movies" className="form-label">
-                                            Search flower
-                                        </label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            name="search_movies"
-                                            onChange={handleChange}
-                                            value={searchValue}
-                                            aria-describedby="search-movies"
-                                        />
-                                    </div>
-                                </div>
-                            </>
-                        );
-                    }}
-                </SearchContext.Consumer>
-
-                }
-
-
-                <div>
-                    <ul className="nav">
-                        <li className="nav-item">
-                            <Link to="/register" className="nav-link">
-                                Sign up
-                            </Link>
-                        </li>
-
-                        {
-                            isAuthenticated ?
-                                <>
-                                    <li className="nav-item">
-                                        <button onClick={handleLogout}  className="btn btn-link text-decoration-none">
-                                            Log out
-                                        </button>
-                                    </li>
-                                </>
-                                :
-
-                                <>
-                                    <li className="nav-item">
-                                        <Link to="/login" className="nav-link">
-                                            Log in
-                                        </Link>
-                                    </li>
-                                </>
-                        }
-
-                        <li className="nav-item">
-                            <Link to="/authenticated-user" className="nav-link">
-                                Authenticated user
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-        </>
-    )
+            <li className="nav-item">
+              <Link to="/authenticated-user" className="nav-link">
+                Authenticated user
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 Header.propTypes = {
-    showSearch: PropTypes.bool
-}
+  showSearch: PropTypes.bool,
+};
 
-export default Header
+export default Header;
